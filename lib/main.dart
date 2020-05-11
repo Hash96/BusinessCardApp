2c2134c6ab3bfeec94be8898c4d54541cc267772
Hash96 <hmhmkumara@students.nsbm.lk>
2020-05-11 08:40:41 +0530

    created micard project

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    myapp()
+  );
+}
+class myapp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    home: Scaffold(
+    backgroundColor: Colors.blue,
+body: Container(),
+),
+);
+  }
+}
