6d9b9d32ef92cef15915744f92393c8e0f320d35
Hash96 <hmhmkumara@students.nsbm.lk>
2020-05-11 13:43:00 +0530

    added a background vatar and headings

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,36 @@ class myapp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
     home: Scaffold(
-    backgroundColor: Colors.blue,
-body: Container(),
+    backgroundColor: Colors.teal,
+body: SafeArea(
+  child: Column(
+    children: <Widget>[
+      CircleAvatar(
+        radius: 50.0,
+        backgroundImage: AssetImage('images/Hashan.jpg'),
+      ),
+      Text(
+          'Hashan Mahisanka',
+        style: TextStyle(
+          fontFamily: 'Pacifico',
+          fontSize: 40.0,
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      Text(
+      'FLUTTER DEVELOPER',
+        style: TextStyle(
+          fontFamily: 'Source Sans Pro',
+          fontSize: 20.0,
+          color: Colors.teal[100],
+          letterSpacing: 2.5,
+          fontWeight: FontWeight.bold,
+        ),
+      )
+    ],
+  ),
+),
 ),
 );
   }
